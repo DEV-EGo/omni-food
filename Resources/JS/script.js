3c0a43914943fbe02b8211863e73f7d3bd0bef8e
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2019-10-01 21:59:37 -0400

    scroll-animation

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -18,4 +18,25 @@ $(document).ready(function () {
         $("html, body").animate({ scrollTop: $(".js--section-plans").offset().top }, 1000);
     });
 
+    $(".js--scroll-to-start").click(function () {
+        $("html, body").animate({ scrollTop: $(".js--section-features").offset().top }, 1000);
+    });
+
+    // *navigation scroll*//
+
+    $(function () {
+        $("a[href*=#]:not([href=#])").click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this, hostname) {
+                var target = $(this.hash);
+                target = target.lenght ? target : $("[name=" + this.hash.slice(1) + "]");
+                if (target.lenght) {
+                    $("html,body").animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
 });
