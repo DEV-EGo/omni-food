954360e5aa71fa26422d4e438bbf4f93cd1c7267
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2019-10-07 18:05:13 -0400

    images-update

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     // *navigation scroll*//
 
     $(function () {
-        $("a[href*=#]:not([href=#])").click(function () {
+        $("a[href=*#]:not([href=*#])").click(function () {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this, hostname) {
                 var target = $(this.hash);
                 target = target.lenght ? target : $("[name=" + this.hash.slice(1) + "]");
